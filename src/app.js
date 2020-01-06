de5450d27957682b28966d42b054cb30fe2d20cd
Robert Bergers <me@robertbergers.com>
2020-01-06 05:39:00 -0800

    Refactor imports

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 // Libraries
 import React from 'react'
 import ReactDOM from 'react-dom'
-import 'normalize.css/normalize.css'
-import 'react-dates/lib/css/_datepicker.css'
 import {Provider} from 'react-redux'
 
-// Assets
+// Styles
 import './sass/main.scss'
+import 'normalize.css/normalize.css'
+import 'react-dates/lib/css/_datepicker.css'
+
+// Assets
 import AppRouter from './routers/AppRouter'
 import configureStore from './store/configureStore'
 import getVisibleExpenses from './selectors/expenses'
