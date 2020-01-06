5e6202e53ed6c70f8b352ec7b504e1b85fb4b226
Robert Bergers <me@robertbergers.com>
2020-01-06 06:37:16 -0800

    add wildcard route

--- server/server.js
+++ server/server.js
@@ -4,8 +4,14 @@ const express = require('express')
 
 // Express config
 const app = express()
+
 const publicPath = path.join(__dirname, '..', 'public')
 app.use(express.static(publicPath))
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(publicPath, 'index.html'))
+})
+
 
 // Run server
 app.listen(3000, () => {
