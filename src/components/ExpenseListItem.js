ab3105c4a7b2facc10c2061dc5b1bf2f88247490
Robert Bergers <me@robertbergers.com>
2020-01-07 10:43:04 -0800

    Format expense dates

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import moment from 'moment'
 
 const ExpenseListItem = ({id, description, amount, createdAt}) => (
   <div>
     <h6>{description}</h6>
-    <p>{amount} | {createdAt}</p>
+    <p>
+        {amount} 
+        | 
+        {moment(createdAt).format('MMM DD YYYY')}
+    </p>
     
     <Link to={`/edit/${id}`}>
         Edit Expense
