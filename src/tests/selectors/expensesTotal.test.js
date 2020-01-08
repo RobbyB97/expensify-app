083068b40dcabf0915c328f51716750d80f982d2
Robert Bergers <me@robertbergers.com>
2020-01-08 14:40:58 -0800

    test expensesTotal with one or no expenses

--- src/tests/selectors/expensesTotal.test.js
+++ src/tests/selectors/expensesTotal.test.js
@@ -2,6 +2,18 @@ import expensesTotal from '../../selectors/expensesTotal'
 import expenses from '../fixtures/expenses'
 
 
+test('expensesTotal with no expenses', () => {
+    const sum = expensesTotal([])
+    expect(sum).toBe(0)
+})
+
+
+test('expensesTotal with one expense', () => {
+    const sum = expensesTotal([expenses[1]])
+    expect(sum).toBe(19500)
+})
+
+
 test('expensesTotal with multiple expenses', () => {
     const sum = expensesTotal(expenses)
     expect(sum).toBe(24195)
