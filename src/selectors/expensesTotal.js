f49174f6d89baddffc22bafe0b98a458b05e5cbe
Robert Bergers <me@robertbergers.com>
2020-01-08 15:03:18 -0800

    refactor: simplify expensesTotal function

--- src/selectors/expensesTotal.js
+++ src/selectors/expensesTotal.js
@@ -1,11 +1,7 @@
 export default (expenses) => {
-    const getTotal = (total, num) => {
-        return total + num
-    }
-
     const expenseArr = expenses.map((amount) => {
         return amount.amount
     })
 
-    return expenseArr.reduce(getTotal, 0)
+    return expenseArr.reduce((total, value) => total + value, 0)
 }
