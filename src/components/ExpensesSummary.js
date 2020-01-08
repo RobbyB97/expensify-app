7455fb51b4ba909a6a44638f6c4ffb0f0c59d725
Robert Bergers <me@robertbergers.com>
2020-01-08 15:29:44 -0800

    add ExpensesSummary content

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,19 +1,26 @@
 import React from 'react'
 import {connect} from 'react-redux'
+import numeral from 'numeral'
 
 import expensesTotal from '../selectors/expensesTotal'
 
 
 export const ExpensesSummary = (props) => (
     <div>
-        
+        {
+            props.expensesCount ?
+                `You have ${props.expensesCount} expenses totalling ${props.expensesTotal}.`
+            :
+            'You have no expenses.'
+        }
     </div>
 )
 
 
 const mapStateToProps = (state) => {
     return {
-        expensesTotal: expensesTotal(state.expenses)
+        expensesTotal: numeral(expensesTotal(state.expenses)/100).format('$0,0.00'),
+        expensesCount:  state.expenses.length
     }
 }
 
