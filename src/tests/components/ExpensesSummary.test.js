33208f6c4e9617ecde0c7826925ad5c79146ecff
Robert Bergers <me@robertbergers.com>
2020-01-09 07:06:28 -0800

    ExpensesSummary test with one and multiple expenses

--- src/tests/components/ExpensesSummary.test.js
+++ src/tests/components/ExpensesSummary.test.js
@@ -5,5 +5,30 @@ import {ExpensesSummary} from '../../components/ExpensesSummary'
 
 test('Render ExpensesSummary with no expenses', () => {
     const wrapper = shallow(<ExpensesSummary />)
+    
+    expect(wrapper).toMatchSnapshot()
+})
+
+
+test('Render ExpensesSummary with one expense', () => {
+    const wrapper = shallow(
+        <ExpensesSummary 
+            expensesCount={1}
+            expensesTotal={'$100.00'}
+        />
+    )
+
+    expect(wrapper).toMatchSnapshot()
+})
+
+
+test('Render ExpensesSummary with multiple expenses', () => {
+    const wrapper = shallow(
+        <ExpensesSummary
+            expensesCount={3}
+            expensesTotal={'$340.60'}
+        />
+    )
+
     expect(wrapper).toMatchSnapshot()
 })
